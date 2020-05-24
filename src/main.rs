a755643ec7cd2fa3d855f20a98d04e5430379d35
Eduardo Riol <eriol@ebay.com>
2020-05-24 22:37:05 +0200

    Responding with a workout in str

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,20 @@
 
 #[get("/")]
 pub fn hello() -> &'static str {
-    "Hello, world!"
+    "Hello, workout app!"
+}
+
+#[get("/workout/<group>")]
+fn workout(group: String) -> Option<&'static str> {
+    if group.as_str() == "legs" {
+        Some("Squats: 4 x 5\nLunges: 3 x 12")
+    } else {
+        None
+    }
 }
 
 pub fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![hello])
+    rocket::ignite().mount("/", routes![hello, workout])
 }
 
 fn main() {
--- src/tests.rs
+++ src/tests.rs
@@ -3,9 +3,24 @@ use rocket::local::Client;
 use rocket::http::Status;
 
 #[test]
-fn hello_world() {
-    let client = Client::new(rocket()).expect("valid rocket instance");
+fn hello_workout_app() {
+    let client = Client::new(rocket()).expect("Failed to create test client");
     let mut response = client.get("/").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    assert_eq!(response.body_string(), Some("Hello, workout app!".into()));
+}
+
+#[test]
+fn get_legs_workout() {
+    let client = Client::new(rocket()).expect("Failed to create test client");
+    let mut response = client.get("/workout/legs").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("Squats: 4 x 5\nLunges: 3 x 12".into()));
+}
+
+#[test]
+fn get_unknown_workout() {
+    let client = Client::new(rocket()).expect("Failed to create test client");
+    let response = client.get("/workout/unknown").dispatch();
+    assert_eq!(response.status(), Status::NotFound);
 }
