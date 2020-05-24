01c0bd2b4114a7ab6bef594e184383c9a359d416
Eduardo Riol <eriol@ebay.com>
2020-05-24 19:23:59 +0200

    First dummy endpoint and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[cfg(test)] mod tests;
+
+#[get("/")]
+pub fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+pub fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![hello])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
