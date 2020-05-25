ba75099757907117a224519399a6217f803f3107
Eduardo Riol <eriol@ebay.com>
2020-05-25 13:13:36 +0200

    Responding with all muscular groups

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,14 @@ pub fn hello() -> &'static str {
 
 #[get("/workout/<group>")]
 fn workout(group: String) -> Option<&'static str> {
-    if group.as_str() == "legs" {
-        Some("Squats: 4 x 5\nLunges: 3 x 12")
-    } else {
-        None
+    match group.as_str() {
+        "legs" => Some("Squats: 4 x 5\nLunges: 3 x 12"),
+        "push" => Some("Barbell bench press: 4 x 5\nOverhead barbell press: 3 x 12"),
+        "pull" => Some("Deadlift: 4 x 5\nPull-ups: 3 x 8"),
+        "upper" => Some("Dumbbell bench press: 4 x 5\nOverhead dumbbell press: 3 x 12"),
+        "lower" => Some("Squats: 4 x 5\nLunges: 3 x 12"),
+        "full" => Some("Barbell bench press: 4 x 5\nOverhead barbell press: 3 x 12\nSquats: 4 x 5\nLunges: 3 x 12"),
+        _ => None,
     }
 }
 
