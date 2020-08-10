c1e45482690aed90b502419b0979b741aceb2d74
Eduardo Riol <eriol@ebay.com>
2020-08-10 13:21:23 +0200

    Adding CORS

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use]
 extern crate diesel;
+extern crate rocket_cors;
 extern crate serde;
 
 pub mod schema;
@@ -14,10 +15,37 @@ mod db;
 #[cfg(test)]
 mod tests;
 
+use rocket::http::Method;
+
 use rocket_contrib::json::JsonValue;
+
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions, Error};
 
 mod models;
 use models::Workout;
+
+fn make_cors() -> Cors {
+    let allowed_origins = AllowedOrigins::some_exact(&[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:8000",
+        "http://0.0.0.0:8000",
+    ]);
+
+    CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin",
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS")
+}
 
 #[get("/")]
 pub fn hello() -> &'static str {
@@ -33,8 +61,10 @@ pub fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .manage(db::connect())
         .mount("/", routes![hello, workouts])
+        .attach(make_cors())
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     rocket().launch();
+    Ok(())
 }
